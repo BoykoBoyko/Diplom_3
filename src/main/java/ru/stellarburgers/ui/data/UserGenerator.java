b7c0a434dd630483a1d65fe603a4d9d02e40a580
Elena Boyko <boooykot@gmail.com>
2023-01-14 23:52:50 +0200

    Добавлены тесты. Повторящиеся Before и After вынесены в отдельный класс Добавлен отчет Allure

---
+++ src/main/java/ru/stellarburgers/ui/data/UserGenerator.java
@@ -0,0 +1,13 @@
+package ru.stellarburgers.ui.data;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+
+public class UserGenerator {
+
+    public static User getNewRandomUser() {
+        return new User(RandomStringUtils.randomAlphanumeric(9),
+                RandomStringUtils.randomAlphanumeric(9) + "@yandex.ru",
+                RandomStringUtils.randomAlphanumeric(9));
+    }
+}
---
+++ src/test/java/LogoutTest.java
@@ -0,0 +1,29 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.stellarburgers.ui.pageobject.MainPage;
+
+import java.time.Duration;
+
+import static ru.stellarburgers.ui.Constants.LOGIN_PAGE_URL;
+
+public class LogoutTest extends BeforeAfterRuleTest {
+
+    @Test
+    @DisplayName("Checking the logout by Выход on the profile page")
+    @Description("Check that the main page is displayed")
+    public void logoutIsSuccessful() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.openMainPage()
+                .clickAccountButton()
+                .authorizationFromLoginPage(user.getEmail(), user.getPassword())
+                .clickLoginEnterButton()
+                .clickProfileButtonFromAuthorizedUser()
+                .clickLogOutButton();
+        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.urlToBe(LOGIN_PAGE_URL));
+        Assert.assertEquals(LOGIN_PAGE_URL, driver.getCurrentUrl());
+    }
+}
